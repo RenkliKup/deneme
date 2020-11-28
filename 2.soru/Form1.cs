d32f3eb95eebe6891fb91a745e4f75f31c4e6eaf
Baris Karakulak <baris.1907.krklk@gmail.com>
2020-11-28 15:12:50 +0300

    ilk commit

---
+++ 2.soru/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+ 
+namespace _2.soru
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            UstHesap yeni = new UstHesap();
+            yeni.get_set = Convert.ToInt32(textBox1.Text);
+            yeni.Hesap();
+            textBox2.Text = yeni.get_set.ToString();
+          
+        }
+    }
+}
---
+++ 2.soru/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.soru
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+    class UstHesap
+    {
+        public int deger;
+        public int sonuc=1;
+        public void Hesap()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                sonuc *= deger;
+            }
+        }
+        public int get_set
+        {
+            get
+            {
+                return sonuc;
+            }
+            set
+            {
+                deger =value;
+            }
+            
+        }
+    }
+}
